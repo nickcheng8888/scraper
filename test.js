2dca8f7b2fb071ecf5502547a8558f0920fb73f5
Nick Cheng <nicholascheng25@gmail.com>
2020-12-10 08:53:40 +1100

    we have a list of job titles

--- test.js
+++ test.js
@@ -7,15 +7,16 @@ const puppeteer = require('puppeteer');
     await page.type('#text-input-what', 'developer');
     const elements = await page.$x('//*[@id="whatWhereFormId"]/div[3]/button')
     await elements[0].click() 
-    //var linkTexts = await page.$$eval(".title",
-    //            elements=> elements.map(item=>item.textContent))
+    await page.waitForSelector('.title');
+    var linkTexts = await page.$$eval(".title",
+                elements=> elements.map(item=>item.textContent));
     await page.screenshot({
         path: "./screenshot.jpg",
         type: "jpeg",
         fullPage: true
     });
     // we have made it into the page where we have job results
-    //await console.log(linkTexts);
+    await console.log(linkTexts);
     await page.close();
     await browser.close(); 
    })()
