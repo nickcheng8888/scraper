eec9b80354f71bc1ec8555c47f0f4de2b70cbcbe
Nicholas Cheng <49749803+nickcheng8888@users.noreply.github.com>
2021-03-10 22:42:27 +1100

    simple scraping tool which displays on terminal

---
+++ test.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+let pythonBridge = require('python-bridge');
+ 
+let python = pythonBridge();
+ 
+
+(async () => {
+    // launch a chrome browser and go to indeed
+    const browser = await puppeteer.launch({
+
+        headless: false,
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        args: ["--window-size=640,1230"]
+
+    });
+    const page = await browser.newPage();
+    await page.goto('https://au.indeed.com/', {waitUntil: 'networkidle2'});
+    // search for developer jobs and print a list of tuples (job, company)
+    await page.type('#text-input-what', 'developer');
+    const elements = await page.$x('//*[@id="whatWhereFormId"]/div[3]/button')
+    await elements[0].click() 
+    await page.waitForSelector('.title');
+    await page.waitForSelector('.company');
+    
+    var jobTitle = await page.$$eval(".title",
+                elements=> elements.map(item=>item.textContent));
+    var companyName = await page.$$eval(".company",
+                elements=> elements.map(item=>item.textContent));      
+    // use python-bridge to join two lists
+    var tuples = python`list(zip(${jobTitle}, ${companyName}))`;    
+    
+    await page.screenshot({
+        path: "./screenshot.jpg",
+        type: "jpeg",
+        fullPage: true
+    }); 
+    
+    // we have made it into the page where we have job results
+    console.log(tuples);
+    await page.close();
+    await browser.close(); 
+   })()
+
+
